2582115bf860c5be352bd6e850d88debf6173aa6
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-06-15 16:56:23 -0300

    [red] passing four as parameter

--- main_test.go
+++ main_test.go
@@ -28,3 +28,16 @@ func TestFizzBuzz_Two(t *testing.T) {
 	// assert
 	assert.Equal(t, strconv.Itoa(input), got)
 }
+
+func TestFizzBuzz_Four(t *testing.T) {
+	// arrange
+	input := 4
+
+	// act
+	got := FizzBuzz(input)
+
+	// assert
+	if got != "4" {
+		t.Errorf(`expected "4" but got %q`, got)
+	}
+}
