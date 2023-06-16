839684e9089b6582afd68b8d1f96646e68ce5727
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-06-16 13:34:09 -0300

    [refactor] Main

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import "strconv"
 
 func FizzBuzz(input int) string {
-
-	if input == 15 {
+	if input%3 == 0 && input%5 == 0 {
 		return "FizzBuzz"
 	}
-
 	if input%5 == 0 {
 		return "Buzz"
 	}
